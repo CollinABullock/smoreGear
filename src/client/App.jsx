10d9f7fea0f8b9e449e7dac4ae63f57a61f0542a
traviskb <travis_kb@yahoo.com>
2023-10-31 08:45:48 -0500

    front end getallproducts

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link, useNavigate, BrowserRouter } from "react-router-dom";
 import AllUsers from './components/allUsers';
 import Login from './components/Login';
+import AllProducts from "./components/AllProducts";
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
 <Routes>
   <Route path="/" element={<Login />} />
   <Route path="/users" element={<AllUsers />} />
-
+  <Route path="/products" element={<AllProducts />} />
 </Routes>
 </BrowserRouter>
 </>
