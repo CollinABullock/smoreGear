03843fd98e69b52e0456f36cd16b06ce170b3710
“CollinABullock” <cbullock@schoolofrock.com>
2023-11-01 10:21:28 -0500

    updated logo

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -12,7 +12,7 @@ function Home() {
     <h1>S'More Gear!</h1>
       <h4>Buy and sell everything you need for the great outdoors</h4>
       <NavBar />
-      <img src="src/client/assets/images/smoregearlogo.png" className="homeIMG" alt="The Great Outdoors" />
+      <img src="src/client/assets/images/smoregearlogo.jpg" className="homeIMG" alt="The Great Outdoors" />
       <p>(c) 2023, Collin A. Bullock, Travis Bergen, Andy Nunez, and Fletcher Burton</p>
       </div>
   )
