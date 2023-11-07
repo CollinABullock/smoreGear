6f0879f2de465846ecfbbcee1c8e65804ad27e47
“CollinABullock” <cbullock@schoolofrock.com>
2023-11-07 11:33:44 -0600

    fixed syntax error in navbar

--- src/client/components/navBar.jsx
+++ src/client/components/navBar.jsx
@@ -5,51 +5,38 @@ function NavBar(props) {
   let navigate = useNavigate();
   return (
     <>
-    <div className="logoDiv">
-    <img src="src/client/assets/images/smoregearlogo.jpg" className="logoPic"/>
-    </div>
-    <h1>Buy and sell everything you need for the great outdoors!</h1>
-    <nav>
-      {props.isLoggedIn ? (
-        <div className="navbar">
-          <Link className="links" to="/">Home</Link>
-          <Link className="links" to="/users">Users</Link>
-          <Link className="links" to="/products">For Sale!</Link>
-          <Link className="links" to="/profile">My Profile</Link>
-          <Link className="links" to="/products/create-post">Sell Something!</Link>
-          <button id="logout-button"
-            onClick={() => {
-              props.setIsLoggedIn(false);
-              localStorage.removeItem("token"); //Removes token from local storage when logout is clicked.
-              navigate("/")
-          }}>Logout
-          </button>
-        </div>
-      ) : (
-
-        <>
-        <div className="navbar">
-          <Link className="links" to="/">
-            Home
-            </Link>
-        
-       
-          <Link className="links" to="/products">
-            See What's For Sale!  
-          </Link>
-          <Link className="links" to="/login">
-            Login  
-          </Link>
-          <Link className="links" to="/register">
-            Register  
-          </Link>
-
-          <Link className="links" to="/shoppingcart">
-            ShoppingCart
-          </Link>
-        </>
-        </div>
-      )}
+      <div className="logoDiv">
+        <img src="/src/client/assets/images/smoregearlogo.jpg" className="logoPic" alt="SmoreGear Logo" />
+      </div>
+      <h1>Buy and sell everything you need for the great outdoors!</h1>
+      <nav>
+        {props.isLoggedIn ? (
+          <div className="navbar">
+            <Link className="links" to="/">Home</Link>
+            <Link className="links" to="/users">Users</Link>
+            <Link className="links" to="/products">For Sale!</Link>
+            <Link className="links" to="/profile">My Profile</Link>
+            <Link className="links" to="/products/create-post">Sell Something!</Link>
+            <button id="logout-button"
+              onClick={() => {
+                props.setIsLoggedIn(false);
+                localStorage.removeItem("token"); // Removes token from local storage when logout is clicked.
+                navigate("/");
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="navbar">
+            <Link className="links" to="/">Home</Link>
+            <Link className="links" to="/products">See What's For Sale!</Link>
+            <Link className="links" to="/login">Login</Link>
+            <Link className="links" to="/register">Register</Link>
+            <Link className="links" to="/shoppingcart">ShoppingCart</Link>
+          </div>
+        )}
+      </nav>
     </>
   );
 }
