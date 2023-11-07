86ac64a591781c3f72d489511cbb884839fdf52e
“CollinABullock” <cbullock@schoolofrock.com>
2023-11-07 10:01:54 -0600

    added mui log in page

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Link, useNavigate, BrowserRouter } from "react-router-dom";
 import AllUsers from "./components/allUsers";
-import Login from './components/Login';
+// import Login from './components/Login';
 import Register from "./components/Register";
 import AllProducts from "./components/allProducts";
 import SingleProduct from "./components/singleProduct";
@@ -9,6 +9,7 @@ import CreatePost from "./components/create-post";
 import NavBar from "./components/navBar";
 import Submission from "./components/submission";
 import Profile from "./components/profile";
+import Login from "./components/SignIn";
 import "./style.css";
 
 
@@ -27,7 +28,7 @@ export default function App() {
     <>
     <BrowserRouter>
 <Routes>
-  <Route path="/" element={<Home />} />
+  <Route path="/" element={<Login />} />
   <Route path="/profile" element={<Profile />} />
   <Route path="/login" element={<Login />} />
   <Route path="/users" element={<AllUsers />} />
